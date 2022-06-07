7b435d836d13e0b8c1ce6accb477972fe0733337
Mr. Alamgir Hossain <74267177+alamgir16@users.noreply.github.com>
2022-06-08 01:29:09 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+</head>
+<body>
+<div id="example">
+
+</div>
+<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
